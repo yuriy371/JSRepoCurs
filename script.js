4887f33bfd26ee2dc251040ffb07f7e0ce7f7286
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-04 14:25:07 +0400

    добовления взаимодействия с пользователем и создание условия

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 "use strict";
 
-let title = "JS";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 35;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let rollback = 66;
-let fullPrice = 500000;
-let adaptive = true;
+let serviceAdd1 = prompt("Какой дополнительный тип услуги нужен?");
+let serviceAddPrice1 = +prompt("Сколько это будет стоить?");
+let serviceAdd2 = prompt("Какой дополнительный тип услуги нужен?");
+let serviceAddPrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + serviceAddPrice1 + serviceAddPrice2;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let servicePercentPrice = fullPrice - (fullPrice * (rollback/100));
 
 // alert("VS Code");
 // console.log('VS Code');
@@ -18,3 +23,14 @@ console.log("Стоимость верстки экранов " + screenPrice + 
 console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
 console.log(screens.toLowerCase().split(", "));
 console.log("Процент отката посреднику за работу " + (fullPrice * (rollback/100)));
+console.log("Итого: " + Math.ceil(servicePercentPrice) + " рублей");
+
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+    console.log("Скидка не предусмотрена");
+} else {
+    console.log("Что то пошло не так");
+}
