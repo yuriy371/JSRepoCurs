e5d497a4bcf99c8e77bf84c77f77b169b1cc84a4
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-06 09:25:23 +0400

    добовление циклов

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 "use strict";
 
-let title = prompt("Как называется ваш проект?");
+let title = prompt("Как называется ваш проект?", "калькулятор верстки");
 let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let rollback = 66;
-let serviceAdd1 = prompt("Какой дополнительный тип услуги нужен?");
-let serviceAddPrice1 = +prompt("Сколько это будет стоить?");
-let serviceAdd2 = prompt("Какой дополнительный тип услуги нужен?");
-let serviceAddPrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice;
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "15000");
+// let serviceAdd1 = prompt("Какой дополнительный тип услуги нужен?");
+// let serviceAddPrice1 = +prompt("Сколько это будет стоить?");
+// let serviceAdd2 = prompt("Какой дополнительный тип услуги нужен?");
+// let serviceAddPrice2 = +prompt("Сколько это будет стоить?");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
+let rollback = 66;
+let fullPrice;
 let servicePercentPrice;
 let allServicePrices;
 
@@ -30,33 +30,41 @@ const getRollbackMessage = function (price) {
 
 }
 
-const getAllServicePrices = function (serviceAddPrice1, serviceAddPrice2) {
-    allServicePrices = serviceAddPrice1 + serviceAddPrice2
+const getAllServicePrices = function () {
+    let sum = 0
+    for (let i = 0; i < 2; i++) {
+        sum += +prompt("Сколько это будет стоить?", "15000")
+    }
+
+    return sum
+    // allServicePrices = serviceAddPrice1 + serviceAddPrice2
 }
 
-const getTitle = function (title) {
+const getTitle = function () {
     title = title[0].trim() + title.slice(1).toLowerCase()
     return title[0].trim().toUpperCase() + title.slice(1).toLowerCase()
 }
 
-const getServicePercentPrices = function (fullPrice, rollback) {
+const getServicePercentPrices = function () {
     return servicePercentPrice = fullPrice - (fullPrice * (rollback / 100))
 }
 
-function getFullPrice(screenPrice, allServicePrices) {
-    fullPrice = screenPrice + allServicePrices
+function getFullPrice() {
+    return fullPrice = screenPrice + allServicePrices
 }
 showTypeOf(title)
 showTypeOf(screenPrice)
 showTypeOf(adaptive)
 
-getAllServicePrices(serviceAddPrice1, serviceAddPrice2)
-getFullPrice(screenPrice, allServicePrices)
-getTitle(title)
-getServicePercentPrices(fullPrice, rollback)
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+title = getTitle()
+servicePercentPrice = getServicePercentPrices()
+
+console.log('allServicePrices', allServicePrices);
 
 console.log(screens.toLowerCase().split(", "))
 console.log(getRollbackMessage(fullPrice))
-console.log("Итого: " + Math.ceil(getServicePercentPrices(fullPrice, rollback)) + " рублей");
+console.log("Итого: " + Math.ceil(getServicePercentPrices()) + " рублей");
 
 console.log(getTitle(title));
