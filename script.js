175bc734c1ffaaf51c996cd0c8916fc9a6357865
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-07 13:39:56 +0400

    финальная версия

--- script.js
+++ script.js
@@ -6,20 +6,46 @@ const isNumber = function (num) {
 
 function hiddenNum() {
     let randomNum = Math.ceil(Math.random() * 100)
+    let numAttempts = 10
 
     function tryToGuess() {
         let question = prompt("Угадай число от 1 до 100")
+        let replay
+
+        function attempts() {
+            return --numAttempts
+        }
 
         if (isNumber(question)) {
             question = +question
-            if (randomNum < question) {
-                alert("Загаданное число меньше");
-                tryToGuess()
-            } else if (randomNum > question) {
-                alert("Загаданное число больше");
-                tryToGuess()
-            } else if (randomNum == question) {
-                alert("Поздравляю, Вы угадали!!!")
+            if (numAttempts > 1) {
+                if (randomNum < question) {
+                    alert("Загаданное число меньше, осталось попыток " + attempts());
+                    console.log(randomNum, question);
+                    tryToGuess()
+                } else if (randomNum > question) {
+                    alert("Загаданное число больше, осталось попыток " + attempts());
+                    console.log(randomNum, question);
+                    tryToGuess()
+                } else if (randomNum == question) {
+                    replay = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")
+
+                    if (replay === true) {
+                        hiddenNum()
+                    } else {
+                        alert("Спасибо за игру!")
+                    }
+                } else {
+                    alert("Что ты сделал???")
+                }
+            } else {
+                replay = confirm("Попытки закончились, хотите сыграть еще?")
+
+                if (replay === true) {
+                    hiddenNum()
+                } else {
+                    alert("Спасибо за игру!")
+                }
             }
         } else {
             if (question === null) {
@@ -28,7 +54,6 @@ function hiddenNum() {
                 alert("Введи число!");
                 tryToGuess()
             }
-
         }
     }
     tryToGuess()
