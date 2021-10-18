cd3dd9d378ac7cf061739456a82e8501c3569645
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-18 14:43:29 +0400

    to do list с использованием localstorage

--- js/script.js
+++ js/script.js
@@ -1,175 +1,69 @@
-"use strict";
+"use strict"
 
-let titleSite = document.getElementsByTagName("h1")[0]
-let calculate = document.getElementsByClassName("handler_btn")[0]
-let reset = document.getElementsByClassName("handler_btn")[1]
-let plus = document.querySelector(".screen-btn")
-let percent = document.querySelectorAll(".other-items.percent")
-let number = document.querySelectorAll(".other-items.number")
-let rollbackInput = document.querySelector(".rollback [type='range']")
-let rollbackSpan = document.querySelector(".rollback .range-value")
+let todoControl = document.querySelector(".todo-control")
+let headerInput = document.querySelector(".header-input")
+let todoList = document.querySelector(".todo-list")
+let todoCompleted = document.querySelector(".todo-completed")
 
-let totalCost = document.getElementsByClassName("total-input")[0]
-let totalScreens = document.getElementsByClassName("total-input")[1]
-let totalCostServices = document.getElementsByClassName("total-input")[2]
-let totalFullCost = document.getElementsByClassName("total-input")[3]
-let totalCostRollback = document.getElementsByClassName("total-input")[4]
+let storage
 
-let screens = document.querySelectorAll(".screen")
-let screensAll = document.querySelector("div.main-controls__views")
+let toDoData = []
 
-let appData = {
-    title: "",
-    screens: [],
-    screenPrice: 0,
-    adaptive: true,
-    rollback: 66,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    servicePricesPercent: 0,
-    servicePricesNumber: 0,
-    servicesPercent: {},
-    servicesNumber: {},
-    totalScreen: 0,
-    init: function () {
-        appData.addTitle()
-        calculate.addEventListener('click', appData.start)
-        plus.addEventListener("click", appData.addScerenBlock)
-        rollbackInput.addEventListener("input", appData.checkRollback)
+let render = function () {
+    todoList.innerHTML = ""
+    todoCompleted.innerHTML = ""
 
-        screensAll.addEventListener("change", appData.checkScreens)
+    toDoData.forEach(function (item, index) {
+        let newLi = document.createElement("li")
 
-        calculate.disabled = "disabled"
-    },
-    addTitle: function () {
-        document.title = titleSite.textContent
-    },
-    start: function () {
-        appData.addScreens()
-        appData.addServices()
+        newLi.classList.add("todo-item")
+        newLi.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            ' <div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>'
 
-        appData.addPrices()
-
-        // appData.logger()
-        appData.showResult()
-    },
-    showResult: function () {
-        totalCost.value = appData.screenPrice
-        totalCostServices.value = appData.servicePricesPercent + appData.servicePricesNumber
-        totalFullCost.value = appData.fullPrice
-        totalCostRollback.value = appData.servicePercentPrice
-        totalScreens.value = appData.totalScreen
-    },
-    checkRollback: function () {
-        rollbackSpan.innerHTML = rollbackInput.value + "%"
-        appData.rollback = +rollbackInput.value
-        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
-        totalCostRollback.value = appData.servicePercentPrice
-    },
-    checkScreens: function () {
-        screens = document.querySelectorAll(".screen")
-        let arrInp = []
-        let arrSel = []
-
-        let inputCheck = document.querySelectorAll("div.main-controls__input > input")
-        let selectCheck = document.querySelectorAll("div.main-controls__select > select")
-
-        for (let inp of inputCheck) {
-            if (inp.value == "") {
-                arrInp.push(inp.value)
-            }
-        }
-
-        for (let sel of selectCheck) {
-            if (sel.value == "") {
-                arrSel.push(sel.value)
-            }
-        }
-
-        if (arrInp.length >= 2 || arrSel.length >= 2) {
-            calculate.disabled = "disabled"
+        if (item.completed) {
+            todoCompleted.append(newLi)
         } else {
-            calculate.disabled = ""
-        }
-    },
-    
-    addScreens: function () {
-        screens = document.querySelectorAll(".screen")
-
-        screens.forEach(function (screen, index) {
-            let select = screen.querySelector("select")
-            let input = screen.querySelector("input")
-            let selectName = select.options[select.selectedIndex].textContent
-
-            appData.screens.push({
-                id: index,
-                name: selectName,
-                price: +select.value * +input.value,
-                count: +input.value
-            })
-        })
-    },
-    addServices: function () {
-        percent.forEach(function (item) {
-            let check = item.querySelector("input[type=checkbox]")
-            let label = item.querySelector("label")
-            let input = item.querySelector("input[type=text]")
-
-            if (check.checked) {
-                appData.servicesPercent[label.textContent] = +input.value
-            }
-        })
-
-        number.forEach(function (item) {
-            let check = item.querySelector("input[type=checkbox]")
-            let label = item.querySelector("label")
-            let input = item.querySelector("input[type=text]")
-
-            if (check.checked) {
-                appData.servicesNumber[label.textContent] = +input.value
-            }
-        })
-    },
-    addScerenBlock: function () {
-        screens = document.querySelectorAll(".screen")
-
-        let cloneScreen = screens[0].cloneNode(true)
-
-        screens[screens.length - 1].after(cloneScreen)
-    },
-    addPrices: function () {
-        appData.screenPrice = appData.screens.reduce(function (sum, cur) {
-            return sum + cur.price
-        }, 0)
-
-        appData.totalScreen = appData.screens.reduce(function (sum, cur) {
-            return sum + cur.count
-        }, 0)
-
-        for (let key in appData.servicesNumber) {
-            appData.servicePricesNumber += appData.servicesNumber[key]
+            todoList.append(newLi)
         }
 
-        for (let key in appData.servicesPercent) {
-            appData.servicePricesPercent += +appData.screenPrice * (appData.servicesPercent[key] / 100)
-        }
+        newLi.querySelector(".todo-complete").addEventListener('click', function () {
+            item.completed = !item.completed
+            render()
+        })
 
-        appData.fullPrice = +appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent
-        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
-    },
-    logger: function () {
-        // console.log("Название проекта: " + appData.title);
-        // console.log("Полная цена: " + appData.fullPrice);
-        // console.log(appData.getRollbackMessage(appData.fullPrice));
-        // console.log(appData.servicePercentPrice);
-        // console.log(appData);
-        console.log(appData.screens);
-        // console.log(appData.totalNumberScreen);
-        // console.log(appData.screenPrice);
-        // for (let key in appData) {
-        //     console.log("Свойства и методы объекта: " + key);
-        // }
-    },
+        newLi.querySelector(".todo-remove").addEventListener('click', function () {
+            toDoData.splice(index, 1)
+            render()
+        })
+
+    })
+    localStorage.setItem("ToDoList", JSON.stringify(toDoData))
 }
 
-appData.init()
+todoControl.addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    let newToDo = {
+        text: headerInput.value,
+        completed: false,
+    }
+    if (newToDo.text === "") {
+        alert("Введите задачу")
+    } else {
+        headerInput.value = ""
+
+        toDoData.push(newToDo)
+        render()
+    }
+    localStorage.setItem("ToDoList", JSON.stringify(toDoData))
+})
+
+if (localStorage.length == 0) {
+    localStorage.setItem("ToDoList", JSON.stringify(toDoData))
+} else {
+    toDoData = JSON.parse(localStorage.getItem("ToDoList"))
+    render()
+}
